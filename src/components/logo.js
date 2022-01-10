0550629bbf73eb05226ed9b7ce189234c68cba7c
Bui Kha Tung Duong <duongbktph13295@fpt.edu.vn>
2022-01-10 12:25:04 +0700

    logo

--- src/components/logo.js
+++ src/components/logo.js
@@ -1,7 +1,7 @@
 const Logo = {
     print() {
         return /* html */ `
-        <img src="./image/FPT_Polytechnic.png" alt="" class="mx-auto" width="150px">
+        <img src="https://upload.wikimedia.org/wikipedia/commons/2/20/FPT_Polytechnic.png" alt="" class="mx-auto" width="150px">
         `;
     },
 };
