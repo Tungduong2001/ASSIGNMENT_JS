197e261afb2fe6c2d7f5529d8bbf91f278e83266
Bui Kha Tung Duong <duongbktph13295@fpt.edu.vn>
2022-01-13 14:51:59 +0700

    fix: edit signup file

--- src/signin/signup.js
+++ src/signin/signup.js
@@ -34,11 +34,11 @@ const Signup = {
                         <path fill-rule="evenodd" d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z" clip-rule="evenodd" />
                     </svg>
                     </span>
-                    Sign in
+                    Sign Up
                 </button>
                 </div>
                 <div class="text-center">
-                    <p>Do you already have an account?<a href="/signup" class="text-blue-600"> Sign up</a></p>
+                    <p>Do you already have an account?<a href="/signin" class="text-blue-600"> Sign in</a></p>
                 </div>
             </form>
             </div>
