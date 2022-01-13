4e6d74a5865daeba4fb7a9bbb988db7ca21b3c3c
Bui Kha Tung Duong <duongbktph13295@fpt.edu.vn>
2022-01-13 13:08:43 +0700

    fix search

--- src/components/search.js
+++ src/components/search.js
@@ -1,11 +1,11 @@
 const Search = {
-    print() {
-        return /* html */`
+  print() {
+    return /* html */`
         <input type="text">
         <input type="submit" value="TÌM KIẾM"
           class="bg-[#282f53] hover:bg-cyan-600 text-white ml-[8px] px-2 border-[0.5px] hover:text-[#8e4924]">
         `;
-    },
+  },
 };
 
 export default Search;
