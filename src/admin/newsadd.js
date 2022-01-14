dd4a57bd57ce677ecde504c7bd683ba58e753b9b
Bui Kha Tung Duong <duongbktph13295@fpt.edu.vn>
2022-01-14 13:58:11 +0700

    fix news add

--- src/admin/newsadd.js
+++ src/admin/newsadd.js
@@ -70,6 +70,9 @@ const Newsadd = {
                         <button type="submit" class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
                             Save
                         </button>
+                        <a href="/admin/news" class="float-left inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                            Back
+                        </a>
                     </div>
                 </div>
                 </form>
